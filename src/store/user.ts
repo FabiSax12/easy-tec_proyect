f40faba340b3981af2c83628b0155ad73d8e97e3
Fabián Vargas <fabidev18@gmail.com>
2024-03-08 16:33:56 -0600

    Zustand User store

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,17 +1,34 @@
 import {create} from "zustand"
 
+interface Semestre {
+  title: string;
+  startDate: string;
+  endDate: string;
+}
+
 interface User {
-  name: string
-  carrier: string
-  avatarImageURL: string
-  notifications: number
+  name: string;
+  carrier: string;
+  avatarImageURL: string;
+  notifications: number;
+  semestres: Semestre[];
+  addSemester: (semestre: Semestre) => void;
 }
 
 const useUserInfo = create<User>((set, get) => ({
   name: "Fabián Vargas",
-  carrier: "Ingenierían en Computación",
+  carrier: "Ingeniería en Computación",
   avatarImageURL: "https://i.pravatar.cc/150?u=a04258114e29026702d",
-  notifications: 2
-}))
+  notifications: 2,
+  semestres: [
+    { title: "Semestre 1", startDate: "05 Feb 2024", endDate: "31 May 2024" },
+    { title: "Semestre 2", startDate: "05 Feb 2024", endDate: "31 May 2024" },
+  ],
+
+  addSemester: (semestre) =>
+    set((state) => ({
+      semestres: [...state.semestres, semestre],
+    })),
+}));
 
 export default useUserInfo
