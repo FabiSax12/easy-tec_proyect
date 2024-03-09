80b97efef0bc600fb757a5e8203ece420be7981b
Fabián Vargas <fabidev18@gmail.com>
2024-03-08 23:23:32 -0600

    Separation of the Course Table component

--- src/ui/nextui/Table/CoursesMainTable.tsx
+++ src/ui/nextui/Table/CoursesMainTable.tsx
@@ -1,31 +1,16 @@
 "use client"
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  Input,
-  Button,
-  DropdownTrigger,
-  Dropdown,
-  DropdownMenu,
-  DropdownItem,
-  Chip,
-  User as Course,
-  Pagination,
-  Selection,
-  ChipProps,
-  SortDescriptor,
-  User
+  Table, TableHeader, TableColumn, TableBody, TableRow, TableCell,
+  Input, Button, DropdownTrigger, Dropdown, DropdownMenu, DropdownItem,
+  Chip, User as Course, Pagination, ChipProps
 } from "@nextui-org/react";
 import { FaPlus, FaChevronDown } from "react-icons/fa6";
 import { HiDotsVertical } from "react-icons/hi";
 import { LuSearch } from "react-icons/lu";
-import {columns, courses, statusOptions} from "@/ui/nextui/Table/data";
+import { columns, courses, statusOptions } from "@/ui/nextui/Table/data";
 import { capitalize } from "@/utils/Capitalize";
+import { useCourses } from "./useCoursesTable";
 
 const statusColorMap: Record<string, ChipProps["color"]> = {
   aprobado: "success",
@@ -33,58 +18,56 @@ const statusColorMap: Record<string, ChipProps["color"]> = {
   cursando: "primary",
 };
 
-const INITIAL_VISIBLE_COLUMNS = ["name", "credits", "semester", "state", "actions"];
-
 type Course = typeof courses[0];
 
-export default function CoursesMainTable() {
-  const [filterValue, setFilterValue] = React.useState("");
-  const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set([]));
-  const [visibleColumns, setVisibleColumns] = React.useState<Selection>(new Set(INITIAL_VISIBLE_COLUMNS));
-  const [statusFilter, setStatusFilter] = React.useState<Selection>("all");
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const [sortDescriptor, setSortDescriptor] = React.useState<SortDescriptor>({
-    column: "age",
-    direction: "ascending",
-  });
+interface Props {
+  filter?: { semester?: number; state?: string };
+}
 
-  const [page, setPage] = React.useState(1);
+const CoursesMainTable: React.FC<Props> = ({ filter }: Props) => {
+  const {
+    filterValue, selectedKeys, visibleColumns, setFilterValue, setSelectedKeys, setVisibleColumns,
+    statusFilter, setStatusFilter, semesterFilter, setSemesterFilter, rowsPerPage, setRowsPerPage,
+    sortDescriptor, setSortDescriptor, page, setPage, items, onClear, onNextPage, onPreviousPage,
+    onRowsPerPageChange, onSearchChange, pages
+  } = useCourses({filter})
 
   const hasSearchFilter = Boolean(filterValue);
 
-  const headerColumns = React.useMemo(() => {
+  const headerColumns = useMemo(() => {
     if (visibleColumns === "all") return columns;
 
-    return columns.filter((column) => Array.from(visibleColumns).includes(column.uid));
+    return columns.filter((column) =>
+      Array.from(visibleColumns).includes(column.uid)
+    );
   }, [visibleColumns]);
 
-  const filteredItems = React.useMemo(() => {
-    let filteredUsers = [...courses];
+  const filteredItems = useMemo(() => {
+    let filteredCourses = [...courses];
 
     if (hasSearchFilter) {
-      filteredUsers = filteredUsers.filter((user) =>
-        user.name.toLowerCase().includes(filterValue.toLowerCase()),
+      filteredCourses = filteredCourses.filter((course) =>
+        course.name.toLowerCase().includes(filterValue.toLowerCase())
       );
     }
-    if (statusFilter !== "all" && Array.from(statusFilter).length !== statusOptions.length) {
-      filteredUsers = filteredUsers.filter((user) =>
-        Array.from(statusFilter).includes(user.state),
+    if (
+      statusFilter !== "all" &&
+      Array.from(statusFilter).length !== statusOptions.length
+    ) {
+      filteredCourses = filteredCourses.filter((course) =>
+        Array.from(statusFilter).includes(course.state)
+      );
+    }
+    if (semesterFilter !== "all") {
+      filteredCourses = filteredCourses.filter(
+        (course) => semesterFilter == course.semester
       );
     }
 
-    return filteredUsers;
-  }, [courses, filterValue, statusFilter]);
+    return filteredCourses;
+  }, [courses, filterValue, statusFilter, semesterFilter]);
 
-  const pages = Math.ceil(filteredItems.length / rowsPerPage);
-
-  const items = React.useMemo(() => {
-    const start = (page - 1) * rowsPerPage;
-    const end = start + rowsPerPage;
-
-    return filteredItems.slice(start, end);
-  }, [page, filteredItems, rowsPerPage]);
-
-  const sortedItems = React.useMemo(() => {
+  const sortedItems = useMemo(() => {
     return [...items].sort((a: Course, b: Course) => {
       const first = a[sortDescriptor.column as keyof Course] as number;
       const second = b[sortDescriptor.column as keyof Course] as number;
@@ -94,80 +77,63 @@ export default function CoursesMainTable() {
     });
   }, [sortDescriptor, items]);
 
-  const renderCell = React.useCallback((course: Course, columnKey: React.Key) => {
-    const cellValue = course[columnKey as keyof Course];
+  const renderCell = useCallback(
+    (course: Course, columnKey: React.Key) => {
+      const cellValue = course[columnKey as keyof Course];
 
-    switch (columnKey) {
-      case "name":
-        return (
-          <Course
-            // avatarProps={{radius: "lg", src: course.avatar}}
-            description={course.teacher}
-            name={cellValue}
-          >
-            {course.teacher}
-          </Course>
-        );
-      case "state":
-        return (
-          <Chip className="capitalize" color={statusColorMap[course.state]} size="sm" variant="flat">
-            {cellValue}
-          </Chip>
-        );
-      case "actions":
-        return (
-          <div className="relative flex justify-end items-center gap-2">
-            <Dropdown>
-              <DropdownTrigger>
-                <Button isIconOnly size="sm" variant="light">
-                  <HiDotsVertical className="text-default-300" />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu>
-                <DropdownItem color="primary" variant="light">Abrir</DropdownItem>
-                <DropdownItem color="success" variant="light">Editar</DropdownItem>
-                <DropdownItem className="text-danger" color="danger" variant="solid">Eliminar</DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </div>
-        );
-      default:
-        return cellValue;
-    }
-  }, []);
+      switch (columnKey) {
+        case "name":
+          return (
+            <Course
+              // avatarProps={{radius: "lg", src: course.avatar}}
+              description={course.teacher}
+              name={cellValue}
+            >
+              {course.teacher}
+            </Course>
+          );
+        case "state":
+          return (
+            <Chip
+              className="capitalize"
+              color={statusColorMap[course.state]}
+              size="sm"
+              variant="flat"
+            >
+              {cellValue}
+            </Chip>
+          );
+        case "actions":
+          return (
+            <div className="relative flex justify-end items-center gap-2">
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button isIconOnly size="sm" variant="light">
+                    <HiDotsVertical className="text-default-300" />
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu>
+                  <DropdownItem color="primary" variant="light">
+                    Abrir
+                  </DropdownItem>
+                  <DropdownItem color="success" variant="light">
+                    Editar
+                  </DropdownItem>
+                  <DropdownItem className="text-danger" color="danger" variant="solid">
+                    Eliminar
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+          );
+        default:
+          return cellValue;
+      }
+    },
+    []
+  );
 
-  const onNextPage = React.useCallback(() => {
-    if (page < pages) {
-      setPage(page + 1);
-    }
-  }, [page, pages]);
-
-  const onPreviousPage = React.useCallback(() => {
-    if (page > 1) {
-      setPage(page - 1);
-    }
-  }, [page]);
-
-  const onRowsPerPageChange = React.useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
-    setRowsPerPage(Number(e.target.value));
-    setPage(1);
-  }, []);
-
-  const onSearchChange = React.useCallback((value?: string) => {
-    if (value) {
-      setFilterValue(value);
-      setPage(1);
-    } else {
-      setFilterValue("");
-    }
-  }, []);
-
-  const onClear = React.useCallback(()=>{
-    setFilterValue("")
-    setPage(1)
-  },[])
-
-  const topContent = React.useMemo(() => {
+  const topContent = useMemo(() => {
     return (
       <div className="flex flex-col gap-4">
         <div className="flex justify-between gap-3 items-end">
@@ -182,9 +148,9 @@ export default function CoursesMainTable() {
           />
           <div className="flex gap-3">
             <Dropdown>
-              <DropdownTrigger className="hidden sm:flex">
+              <DropdownTrigger className="sm:flex">
                 <Button endContent={<FaChevronDown className="text-small" />} variant="flat">
-                  Status
+                  Estado
                 </Button>
               </DropdownTrigger>
               <DropdownMenu
@@ -203,9 +169,9 @@ export default function CoursesMainTable() {
               </DropdownMenu>
             </Dropdown>
             <Dropdown>
-              <DropdownTrigger className="hidden sm:flex">
+              <DropdownTrigger className="sm:flex">
                 <Button endContent={<FaChevronDown className="text-small" />} variant="flat">
-                  Columns
+                  Columnas
                 </Button>
               </DropdownTrigger>
               <DropdownMenu
@@ -254,7 +220,7 @@ export default function CoursesMainTable() {
     hasSearchFilter,
   ]);
 
-  const bottomContent = React.useMemo(() => {
+  const bottomContent = useMemo(() => {
     return (
       <div className="py-2 px-2 flex justify-between items-center">
         <span className="w-[30%] text-small text-default-400">
@@ -320,4 +286,7 @@ export default function CoursesMainTable() {
       </TableBody>
     </Table>
   );
-}
+};
+
+export default CoursesMainTable;
+
