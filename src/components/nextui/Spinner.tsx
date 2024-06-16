f271c38e4a24e87eea14e30743dc36110836c09d
Fabián Vargas <fabidev18@gmail.com>
2024-06-16 01:33:35 -0600

    Center the spinner in the div

--- src/components/nextui/Spinner.tsx
+++ src/components/nextui/Spinner.tsx
@@ -2,7 +2,7 @@ import { Spinner as SpinnerUI } from "@nextui-org/react"
 
 export const Spinner = () => {
   return (
-    <div className="flex w-full h-full">
+    <div className="flex w-full h-full justify-center items-center">
       <SpinnerUI color="primary" className="items-center justify-center" />
     </div>
   )
