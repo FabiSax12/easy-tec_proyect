41da13b49b9dbe9b1c2f388246f344ce74d9669d
Fabián Vargas <fabidev18@gmail.com>
2024-06-16 01:21:50 -0600

    Web scrapping to get schedules
    - Add web scrapping to get the schedules from TEC Digital, and add api endpoint

---
+++ src/app/api/schedule/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { getSchedule } from '@/libs/scrapping/schedules-scrapping'
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams
+  const campus = searchParams.get("campus")
+  const carrier = searchParams.get("carrier")
+  const period = searchParams.get("period")
+
+  if (campus && carrier && period) {
+    const schedule = await getSchedule(
+      campus,
+      carrier,
+      period,
+      {
+        email: process.env.TEC_EMAIL,
+        password: process.env.TEC_PASSWORD
+      }
+    )
+    return NextResponse.json({ data: schedule }, { status: 200 })
+  } else {
+    return NextResponse.json({ message: "Missing required parameters" }, { status: 400 })
+  }
+}
---
+++ src/interfaces/api-data/schedule.ts
@@ -0,0 +1,14 @@
+export interface ScheduleRow {
+  id: string
+  code: string
+  subject: string
+  group: number
+  credits: number
+  schedule: string
+  classroom: string
+  teacher: string
+  totalSpaces: number
+  typeOfSubject: string
+  typeOfGroup: string
+  reserved: number
+}
