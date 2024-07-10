fa31e800b1d551b7f0091136d6a91e7e3de2eef1
Fabián Vargas <fabidev18@gmail.com>
2024-07-09 22:44:42 -0600

    Refactor Sidebar code

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,17 +2,28 @@ import { SidebarItem } from "./SidebarItem"
 import { SidebarWrapper } from "./SidebarWrapper"
 import { MdDashboard } from "react-icons/md"
 import { RiCalendarEventLine } from "react-icons/ri"
-import {
-  IoCalendarOutline, IoListOutline, IoSettingsOutline
-} from "react-icons/io5"
+import { FiClock } from "react-icons/fi"
+import { IoCalendarOutline, IoListOutline, IoSettingsOutline } from "react-icons/io5"
+
+const items = [
+  { icon: <MdDashboard size={25} />, text: "Dashboard", href: "/principal/dashboard" },
+  { icon: <IoCalendarOutline size={25} />, text: "Calendario", href: "/principal/calendario" },
+  { icon: <RiCalendarEventLine size={25} />, text: "Horarios", href: "/principal/horarios/crear" },
+  { icon: <FiClock size={25} />, text: "Entregas", href: "/principal/entregas" },
+  { icon: <IoListOutline size={25} />, text: "Guias", href: "/principal/guias" },
+]
 
 export const Sidebar = () => {
   return <SidebarWrapper>
-    <SidebarItem icon={<MdDashboard size={25} />} text="Dashboard" href="/principal/dashboard" />
-    <SidebarItem icon={<IoCalendarOutline size={25} />} text="Calendario" href="/principal/calendario" />
-    <SidebarItem icon={<RiCalendarEventLine size={25} />} text="Horarios" href="/principal/horarios/crear" />
-    <SidebarItem icon={<IoListOutline size={25} />} text="Entregas" href="/principal/entregas" />
+    {items.map((item, index) => (
+      <SidebarItem key={index} icon={item.icon} text={item.text} href={item.href} />
+    ))}
+
     <hr />
-    <SidebarItem icon={<IoSettingsOutline size={25} />} text="Configuración" href="/principal/configuracion" />
+    <SidebarItem
+      icon={<IoSettingsOutline size={25} />}
+      text={"Configuración"}
+      href={"/principal/configuracion"}
+    />
   </SidebarWrapper>
 }
