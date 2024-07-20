fbeff7e168656435964926aa3741eb8615377eef
Fabián Vargas <fabidev18@gmail.com>
2024-07-19 18:01:46 -0600

    Update Spinner component to accept props

--- src/components/nextui/Spinner.tsx
+++ src/components/nextui/Spinner.tsx
@@ -1,9 +1,9 @@
-import { Spinner as SpinnerUI } from "@nextui-org/react"
+import { SpinnerProps, Spinner as SpinnerUI } from "@nextui-org/react"
 
-export const Spinner = () => {
+export const Spinner = (props: SpinnerProps) => {
   return (
     <div className="flex w-full h-full justify-center items-center">
-      <SpinnerUI color="primary" className="items-center justify-center" />
+      <SpinnerUI color="primary" className="items-center justify-center" {...props} />
     </div>
   )
 }
