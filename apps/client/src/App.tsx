22d19e89ce85ff9af656f527fa014faceba08bfb
Fabián Vargas <fabidev18@gmail.com>
2024-10-09 16:45:05 -0600

    Refactor routes and App file

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,51 @@
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import { DashboadLayout, AuthLayout } from "@/layouts"
+import { DashboardPage, AuthPage } from "@/pages"
 import "./App.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/principal/dashboard" />,
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "",
+        element: <AuthPage />,
+      }
+    ]
+  },
+  {
+    path: "/principal",
+    element: <DashboadLayout />,
+    children: [
+      {
+        path: "dashboard",
+        element: <DashboardPage />,
+      },
+      {
+        path: "calendario",
+      },
+      {
+        path: "horario",
+      },
+      {
+        path: "config",
+      },
+      {
+        path: "",
+        element: <Navigate to="/principal/dashboard" />,
+      }
+    ]
+  }
+])
 
 function App() {
   return (
-    <>
-      App
-    </>
+    <RouterProvider router={router} />
   )
 }
 
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,58 +1,16 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
-import { DashboadLayout, AuthLayout } from "@/layouts"
-import { DashboardPage, AuthPage } from "@/pages"
+import { AuthProvider } from "@/context/auth"
 import { NextUIProvider } from "@nextui-org/react"
+import App from "./App"
 import "./index.css"
-import { AuthProvider } from "./context/auth"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/principal/dashboard" />,
-  },
-  {
-    path: "/auth",
-    element: <AuthLayout />,
-    children: [
-      {
-        path: "",
-        element: <AuthPage />,
-      }
-    ]
-  },
-  {
-    path: "/principal",
-    element: <DashboadLayout />,
-    children: [
-      {
-        path: "dashboard",
-        element: <DashboardPage />,
-      },
-      {
-        path: "calendario",
-      },
-      {
-        path: "horario",
-      },
-      {
-        path: "config",
-      },
-      {
-        path: "",
-        element: <Navigate to="/principal/dashboard" />,
-      }
-    ]
-  }
-])
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <NextUIProvider>
-        <RouterProvider router={router} />
+        <App />
       </NextUIProvider>
     </AuthProvider>
-  </StrictMode>,
+  </StrictMode>
 )
