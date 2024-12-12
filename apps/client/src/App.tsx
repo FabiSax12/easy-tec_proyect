9f33ba25c4048184cc621246e5ade9d0005d3361
Fabián Vargas <fabidev18@gmail.com>
2024-12-12 09:06:59 -0600

    Refactor router from createBrowserRouter api to BrowserRouter component

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,71 +1,31 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { DashboadLayout, AuthLayout } from "@/layouts"
-import { AuthPage, DashboardPage, NotFound, Period, ProtectedPage, SchedulesPage } from "@/pages"
+import { AuthPage, DashboardPage, NotFound, Period, ProtectedPage, SchedulesPage, GuidesPage } from "@/pages"
 import "./App.css"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/principal/dashboard" replace />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/auth",
-    element: <AuthLayout />,
-    children: [
-      {
-        path: "",
-        element: <AuthPage />,
-      }
-    ]
-  },
-  {
-    path: "/principal",
-    element: <ProtectedPage />,
-    children: [
-      {
-        path: "",
-        element: <DashboadLayout />,
-        children: [
-          {
-            path: "dashboard",
-            element: <DashboardPage />,
-          },
-          {
-            path: "periodo/:id",
-            element: <Period />
-          },
-          {
-            path: "calendario",
-          },
-          {
-            path: "horario",
-            element: <SchedulesPage />
-          },
-          {
-            path: "cursos",
-          },
-          {
-            path: "guias"
-          },
-          {
-            path: "config",
-          },
-          {
-            path: "*",
-            element: <Navigate to="/principal/dashboard" />,
-          }
-        ]
-      }
-    ]
-
-  }
-])
-
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <Routes>
+    <Route path="/" element={<Navigate to="/principal/dashboard" replace />} />
+    <Route path="*" element={<NotFound />} />
+
+    <Route path="/auth" element={<AuthLayout />}>
+      <Route path="" element={<AuthPage />} />
+    </Route>
+
+    <Route path="/principal" element={<ProtectedPage />}>
+      <Route path="" element={<DashboadLayout />}>
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="periodo/:id" element={<Period />} />
+        <Route path="calendario" />
+        <Route path="horario" element={<SchedulesPage />} />
+        <Route path="cursos" />
+        <Route path="guias" element={<GuidesPage />} />
+        <Route path="guias/:slug" element={<GuidesPage />} />
+        <Route path="config" />
+        <Route path="*" element={<Navigate to="/principal/dashboard" />} />
+      </Route>
+    </Route>
+  </Routes>
 }
 
 export default App
