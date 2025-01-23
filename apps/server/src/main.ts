896807e5940616ae59d0595f083a185f46248a37
Fabián Vargas <fabidev18@gmail.com>
2025-01-22 22:48:23 -0600

    Add cookie-parser middleware and update global interceptors in main.ts

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from "@nestjs/core"
-import { AppModule } from "./app.module"
-import { AuthGuard } from "./shared/guards/auth.guard"
 import { ValidationPipe } from "@nestjs/common"
+import * as cookieParser from "cookie-parser"
+import { AppModule } from "./app.module"
 import { BadConnectionSimulatorInterceptor } from "./shared/interceptors/badConnectionSimulator.interceptor"
+import { LoggerInterceptor } from "./shared/interceptors/logger.interceptor"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix("api")
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+  app.use(cookieParser())
   app.useGlobalInterceptors(new BadConnectionSimulatorInterceptor())
-  app.useGlobalGuards(app.get(AuthGuard))
+  app.useGlobalInterceptors(new LoggerInterceptor())
+  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
   await app.listen(3000)
 }
 bootstrap()
