3a1b85f3be9db2cfcf45e7c9ec922b07cd43ffc6
Fabián Vargas <fabidev18@gmail.com>
2025-01-31 20:29:44 -0600

    Implement caching in schedules controller; integrate cache manager for optimized data retrieval

--- apps/server/src/schedules/schedules.controller.ts
+++ apps/server/src/schedules/schedules.controller.ts
@@ -1,15 +1,36 @@
-import { Controller, Get, Query } from "@nestjs/common"
+import { Controller, Get, Inject, Query } from "@nestjs/common"
 import { ScheduleService } from "./schedules.service"
+import { Cache } from "cache-manager"
 
 @Controller("schedules")
 export class SchedulesController {
-  constructor(private readonly scheduleService: ScheduleService) { }
+  constructor(
+    private readonly scheduleService: ScheduleService,
+    @Inject("CACHE_MANAGER") private cacheManager: Cache
+  ) { }
 
   @Get()
-  findAll(@Query("campus") campus: string, @Query("carrier") carrier: string, @Query("period") period: string) {
-    return this.scheduleService.getSchedule(campus, carrier, period, {
+  async findAll(
+    @Query("campus") campus: string,
+    @Query("carrier") carrier: string,
+    @Query("period") period: string
+  ) {
+    const cacheKey = `${campus}-${carrier}-${period}`
+    const cachedData = await this.cacheManager.get(cacheKey)
+
+    if (cachedData) {
+      console.log("Returning cached data")
+      return cachedData  // Returns cached data
+    }
+
+    const data = await this.scheduleService.getSchedule(campus, carrier, period, {
       email: process.env.TEC_EMAIL,
       password: process.env.TEC_PASSWORD
     })
+
+    await this.cacheManager.set(cacheKey, data)  // Save in cache
+
+    console.log("Returning fresh data")
+    return data
   }
 }
