7c3adf86991c3da6d940a960dda425d2c16254d0
Fabián Vargas <fabidev18@gmail.com>
2025-05-06 13:45:48 -0600

    feat: upgrade React and related dependencies, implement MainLayout with navigation and alert

---
+++ apps/scheduler/src/layouts/MainLayout.tsx
@@ -0,0 +1,42 @@
+import { CustomNavLink } from "@/components/ui/CustomNavLink";
+import { SchedulesProvider } from "@/context/schedule";
+import { Alert, HeroUIProvider } from "@easy-tec/ui";
+import { NavigateOptions, Outlet, To, useHref, useNavigate } from "react-router";
+
+export function MainLayout() {
+  const navigateHook = useNavigate();
+  const hrefHook = useHref;
+
+  const handleHideAlert = () => {
+    localStorage.setItem("hideAlert", "true")
+  }
+
+  return (
+    <HeroUIProvider
+      navigate={(to: To, options?: NavigateOptions) => navigateHook(to, options)}
+      useHref={(to: To) => hrefHook(to)}
+    >
+      <SchedulesProvider>
+        <Alert
+          isClosable
+          isDefaultVisible={localStorage.getItem("hideAlert") !== "true"}
+          onClose={handleHideAlert}
+          color='warning'
+          variant='flat'
+          title="Disclaimer"
+          className='mb-2'
+        >
+          <p>Esta página no es oficial del Tecnológico de Costa Rica (TEC), es mantenida de manera independiente por un estudiante.</p>
+          <p>Se recomienda validar siempre los horarios mostrados aquí con el gestor de horarios oficial. Esta herramienta está diseñada únicamente para facilitar la construcción de su horario.</p>
+          <p>Si encuentra algún error, por favor, comuníquelo a la siguiente dirección de correo electrónico: <a href="mailto:f.vargas.1@estudiantec.cr">f.vargas.1@estudiantec.cr</a>. Gracias :)</p>
+        </Alert>
+
+        <div className="mb-4 flex gap-2">
+          <CustomNavLink to={"/manual"}>Manual</CustomNavLink>
+          <CustomNavLink to={"/auto"}>Automático</CustomNavLink>
+        </div>
+        <Outlet />
+      </SchedulesProvider>
+    </HeroUIProvider>
+  );
+}
--- apps/scheduler/src/main.tsx
+++ apps/scheduler/src/main.tsx
@@ -1,16 +1,38 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router"
-import App from "./App.tsx"
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router"
+import { SchedulesPage } from "./pages/SchedulesPage"
+import { AutoSchedulesPage } from "./pages/AutoSchedulesPage"
+import { MainLayout } from "./layouts/MainLayout"
 import "./index.css"
-import { SchedulesProvider } from "./context/schedule.tsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        element: <Navigate to={"/manual"} replace />,
+      },
+      {
+        path: "/manual",
+        Component: SchedulesPage
+      },
+      {
+        path: "/auto",
+        Component: AutoSchedulesPage
+      },
+      {
+        path: "*",
+        element: <Navigate to="/manual" replace />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <SchedulesProvider>
-        <App />
-      </SchedulesProvider>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
